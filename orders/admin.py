ed438a7e38d7ab5878b15ed94f09af265def6767
Emre Yoleri <emreyoleri01@hotmail.com>
2023-02-10 03:04:40 +0300

    Customizing the Django Admin [Order Model]

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id','flavour','size','order_status','size']
+    list_filter=['placed_at','updated_at','order_status']
+
+
+admin.site.register(Order,OrderAdmin)
+
