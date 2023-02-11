94066f23546f09d54dfa01ce1dd2962460873e7e
Emre Yoleri <emreyoleri01@hotmail.com>
2023-02-11 04:07:29 +0300

    Retrieve all orders

--- orders/api/urls.py
+++ orders/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HelloOrdersView
+from orders.api.views import  OrderCreateListView
+# from orders.api.views import  HelloOrdersView
 
 urlpatterns = [
-    path('', HelloOrdersView.as_view(), name='hello-orders')
+    # path('', HelloOrdersView.as_view(), name='hello-orders'),
+    path('', OrderCreateListView.as_view() , name='orders')
 ]
--- orders/api/views.py
+++ orders/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
+from orders.api.serializers import OrderCreationSerializer
+from orders.models import Order
 
 
 class HelloOrdersView(generics.GenericAPIView):
@@ -9,13 +11,22 @@ class HelloOrdersView(generics.GenericAPIView):
 
         return Response(data={'message': 'Hello Orders'},  status=status.HTTP_200_OK)
 
+
 class OrderCreateListView(generics.GenericAPIView):
+
+    serializer_class = OrderCreationSerializer
+
+    queryset = Order.objects.all()
 
     def get(self, request):
 
-        pass
+        orders = Order.objects.all()
 
-    def post (self,request):
+        serializer = self.serializer_class(instance=orders, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
 
         pass
 
@@ -25,12 +36,11 @@ class OrderDetailView(generics.GenericAPIView):
     def get(self, request, order_id):
 
         pass
-    
-    def put(self,request , order_id):
+
+    def put(self, request, order_id):
 
         pass
 
-    def delete(self,request , order_id):
+    def delete(self, request, order_id):
 
         pass
-    
