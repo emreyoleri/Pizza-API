5ff6b96a348fcb8d21733881e11e13d760087aba
Emre Yoleri <emreyoleri01@hotmail.com>
2023-03-16 20:02:52 +0300

    remove api folder

--- orders/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from orders.api.views import  OrderCreateListView
-# from orders.api.views import  HelloOrdersView
-
-urlpatterns = [
-    # path('', HelloOrdersView.as_view(), name='hello-orders'),
-    path('', OrderCreateListView.as_view() , name='orders')
-]
