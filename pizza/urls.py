95fe6463095ffc304ac3708121be4d63b0bd7eca
Emre Yoleri <emreyoleri01@hotmail.com>
2023-03-17 03:15:18 +0300

    update packages

--- pizza/urls.py
+++ pizza/urls.py
@@ -3,8 +3,7 @@ from django.urls import path,include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -27,3 +26,5 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
